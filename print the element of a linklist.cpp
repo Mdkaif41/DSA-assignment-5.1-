2f1efda3829f42dd14fd8f061cb914bd522cc0a2
Mdkaif41 <114006739+Mdkaif41@users.noreply.github.com>
2023-02-13 12:44:43 +0530

    Create print the element of a linklist.cpp

---
+++ print the element of a linklist.cpp
@@ -0,0 +1,13 @@
+void printLinkedList(SinglyLinkedListNode* head) {
+if(head==NULL){
+        return ;
+    }
+    else{
+        SinglyLinkedListNode*p;
+        p=head;
+        while(p!=NULL){
+            cout<<p->data<<"\n";
+            p=p->next;
+        }
+    }
+}
